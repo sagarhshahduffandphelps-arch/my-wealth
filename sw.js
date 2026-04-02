f6a31ad6fd17f28e7ee12a4269a37faabde1de85
sagarhshahduffandphelps-arch <sagar.hshah.duffandphelps@gmail.com>
2026-04-02 10:47:50 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+var CACHE = 'mywealth-v2';
+var FILES = [
+  '/my-wealth/index.html',
+  '/my-wealth/manifest.json',
+  '/my-wealth/icon-192.png',
+  '/my-wealth/icon-512.png'
+];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(
+    caches.open(CACHE).then(function(c){ return c.addAll(FILES); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE; })
+            .map(function(k){ return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e){
+  e.respondWith(
+    fetch(e.request).catch(function(){
+      return caches.match(e.request);
+    })
+  );
+});
